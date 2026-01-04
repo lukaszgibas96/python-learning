b270940363f30fd906e5c9ca26ab1d5f3ec90ec6
Lukasz <lukasz.gibas96@gmail.com>
2026-01-04 21:50:02 +0100

    Add example of match--case usage

---
+++ Day 2 /cs50p_day_2_house_MATCH_syntax.py
@@ -0,0 +1,29 @@
+name = input("what's your name?")
+
+match name:
+    
+    case "Lukasz" | "Piotr" |   "Adam":
+        print("Krakow")
+    # case "Piotr":
+    #     print("Krakow")
+    # case "Adam":
+    #     print("Krakow")
+    case "Bartek":
+        print("Konin")
+    case _:
+        print("Who?")
+
+
+
+# if name == "Lukasz":
+#     print("Krakow")
+# elif name == "Piotr":
+#     print("Krakow")
+# elif name == "Adam":
+#     print("Krakow")
+# elif name == "Bartek":
+#     print("Konin")
+# else:
+#     print("who?")
+
+    
