93d88ccc32133d8cbb2007e8bc7d1c632739c834
Lukasz <lukasz.gibas96@gmail.com>
2026-01-04 21:10:38 +0100

    Add parity checking implemented in multiple ways

---
+++ Day 2 /cs50p_day2_parity.py
@@ -0,0 +1,33 @@
+
+def main():
+    x = int(input("What's x?\n"))
+
+    if is_even(x):
+        print("EVEN")
+    else:
+        print("ODD")
+
+#Parity function version 1 
+
+#   def is_even(n):
+#     if n % 2 ==0:
+#         return True
+#     else:
+#         return False
+
+
+
+# Parity function version 2
+
+#   def is_even(n):
+#     return True if n % 2 == 0 else False
+
+# Parity function version 3
+
+def is_even(n):
+    return n % 2 ==0
+    
+
+
+main()
+
