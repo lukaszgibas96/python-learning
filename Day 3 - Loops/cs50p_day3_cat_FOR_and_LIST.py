b683e74968ca15b89693807b3e2af2d01acbd844
Lukasz <lukasz.gibas96@gmail.com>
2026-01-05 11:32:49 +0100

    Day 3 of Python Learning - Loops

---
+++ Day 3 - Loops/cs50p_day3_cat_FOR_and_LIST.py
@@ -0,0 +1,14 @@
+# List definition in square parentheses
+for i in [0, 1, 2]:
+    print("meow")
+
+# range() definition
+for i in range(3):
+    print("meow")
+
+# if variable is not used in later code, _ could be use only for syntax correctness
+
+for _ in range(3):
+    print("meow")
+
+print("whoof\n" * 3, end="")
