1ef7f66893e0c6ee966713d9415c94a02758ae4a
Lukasz <lukasz.gibas96@gmail.com>
2026-01-06 17:31:36 +0100

    Add 2nd method of number rounding

---
+++ Day 1/cs50p_calculator.py
@@ -0,0 +1,31 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+
+
+
+#_______________Round number
+
+#_____ Method 1 
+# z = round(x / y, 2)
+
+# print(z)
+
+#_____ Method 2 
+
+z = x / y
+
+print(f"{z:.2f}")
+
+
+
+##############################
+# # Round the number
+
+# z = round(x+y)
+
+# # Print the number with "," separator for thousand part
+
+# print(f"{z:,}")
+
+##############################
