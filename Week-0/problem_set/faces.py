499cc642df1956b06fdf2fb25b8d7dc452195b39
Lukasz <lukasz.gibas96@gmail.com>
2026-01-07 19:28:36 +0100

    Problem set 0 - making faces

---
+++ Week-0/problem_set/faces.py
@@ -0,0 +1,15 @@
+# Problem set 0 - making faces
+
+def main():
+
+    text = input()
+    
+    print(convert(text))
+
+def convert(x):
+       return x.replace(":)" , "🙂").replace(":(" , "🙁" )
+
+
+#🙁
+#🙂
+main()
