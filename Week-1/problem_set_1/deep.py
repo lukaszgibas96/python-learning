ef826692155619b9056e3feee84aa5fceb0f640e
Lukasz <lukasz.gibas96@gmail.com>
2026-01-08 20:12:21 +0100

    Problem set 1 - Deep Though

---
+++ Week-1/problem_set_1/deep.py
@@ -0,0 +1,10 @@
+# Problem set 1 - Deep Though
+
+answer = input("What's the Answer to the Great Question of Life, the Universe, and Everything?")
+
+if answer == "42" or answer == "forty-two" or answer == "forty two":
+
+    print("Yes!")
+
+else:
+    print("No")
