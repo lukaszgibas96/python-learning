84288a1f5bf126f71d1b506c6e036132d5f9afd7
Lukasz <lukasz.gibas96@gmail.com>
2026-01-08 22:03:21 +0100

    Problem Set 1 - Home Federal Saving Bank

---
+++ Week-1/problem_set_1/bank.py
@@ -0,0 +1,17 @@
+#
+greeting = input("Greeting: ").lstrip().lower()
+
+word = greeting.find("hello")
+
+char = greeting.find("h")
+
+if word == 0:
+    print("0$")
+
+elif word != 0 and char == 0:
+    print("20$")
+
+else:
+    print("100$")
+
+
