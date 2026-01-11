058308e20362deb84e8714f4ee9c9a041f0cbb11
Lukasz <lukasz.gibas96@gmail.com>
2026-01-11 17:57:55 +0100

    Problem Set 1 - Math Interpreter

---
+++ Week-1/problem_set_1/interpreter.py
@@ -0,0 +1,26 @@
+# Problem Set 1 - Math Interpreter
+
+elements = (input("Expression: ")).split(" ")
+
+print(elements)
+
+x = float(elements[0])
+z = float(elements[2])
+
+if  elements[1] == "-" :
+    result = x - z 
+
+elif elements[1] == "+" :
+    result = x + z 
+
+elif elements[1] == "*" :
+    result = x * z 
+
+elif elements [1] == "/" :
+    result = x / z
+else:
+    print("Wrong mathematical operator")
+
+
+print(f"{result:.1f}")
+
