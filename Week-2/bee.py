620d240e2bc8b4eafa65374afbb5108986b73e80
Lukasz <lukasz.gibas96@gmail.com>
2026-01-12 22:51:44 +0100

    Dictionary Methods from shorts

---
+++ Week-2/bee.py
@@ -0,0 +1,31 @@
+# 
+WORDS = {"PAIR": 4 , "HAIR": 4 , "CHAIR": 5, "GRAPHIC": 7}
+
+
+def main():
+    print("Welcome to Spelling Bee!")
+    print("Your letters are: A I P C R H G")
+
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} left!")
+        guess = input("Guess a word: ")
+
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You've won!!!")
+
+        if guess in WORDS.keys():
+            points = WORDS.pop(guess)
+            print(f"Good job! You scored {points} points.")
+
+    print("Thats the game!")
+
+
+    # Show the points for each word.
+    #WORDS = {"PAIR": 4 , "HAIR": 4 , "CHAIR": 5, "GRAPHIC": 7}
+
+    #for word, points in WORDS.item():
+    #    print(f"{word} was worth {points} points")
+    
+main()
+
