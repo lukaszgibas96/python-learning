c4d24b5184327ef3ba24f5f1ccbb8549763345e1
Lukasz <lukasz.gibas96@gmail.com>
2026-01-12 22:55:57 +0100

    Dictionary Methods from shorts v2

---
+++ Week-2/bee_v2.py
@@ -0,0 +1,11 @@
+# Dictionary Methods from Shorts            /.item()
+WORDS = {"PAIR": 4 , "HAIR": 4 , "CHAIR": 5, "GRAPHIC": 7}
+
+
+def main():
+   for word, points in WORDS.items():
+      
+      print(f"{word} was worth {points} points.")
+
+
+main()
