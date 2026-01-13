78cadd0c33baca48a89e3dfb8beec6db34c91824
Lukasz <lukasz.gibas96@gmail.com>
2026-01-13 20:16:36 +0100

    Lists from shorts v1

---
+++ Week-2/results.py
@@ -0,0 +1,18 @@
+results = ["Mario", "Luigi"]
+
+results.append("Princess")
+results.append("Yoshi")
+results.append("Koopa Troopa")
+results.append("Toad")
+
+# .append add list as a sub-list 
+results.append(["Bowser", "Donkey Kong Jr."])
+print(results)
+
+# remove sub-list
+results.remove(["Bowser", "Donkey Kong Jr."])
+print(results)
+
+# extend "results" list
+results.extend(["Bowser", "Donkey Kong Jr."])
+print(results)
