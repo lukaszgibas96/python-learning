e2479b634fa5ce5d984757ae2c32e442d409a5a3
Lukasz <lukasz.gibas96@gmail.com>
2026-01-13 17:31:13 +0100

    For Loops from shorts v1

---
+++ Week-2/letters.py
@@ -0,0 +1,21 @@
+def main():
+    print(write_letter("Mario", "Princess Peach"))
+    print(write_letter("Luigi", "Princess Peach"))
+    print(write_letter("Daisy", "Princess Peach"))
+    print(write_letter("Yoshi", "Princess Peach"))
+
+def write_letter(receiver, sender):
+
+    return f"""
+
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+    Dear {receiver},
+
+    You are cordially invited to a ball at
+    Peach's Castle this evening, 7:00 P.M.
+
+    Sincerely,
+    {sender}
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+    """
+main()
