09c807d752424ee4bd5b9631859cd861f58cfef9
Lukasz <lukasz.gibas96@gmail.com>
2026-01-13 20:23:23 +0100

    Lists from shorts v2

---
+++ Week-2/results_v2.py
@@ -0,0 +1,16 @@
+# Lists                /.
+
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Koopa Troopa", "Toad", "Bowser", "Donkey Kong Jr."]
+
+results.remove("Bowser")
+
+print(results)
+
+# insert Bowser into list with specific index
+results.insert(0, "Bowser")
+print(results)
+
+# reverse the list
+
+results.reverse()
+print(results)
