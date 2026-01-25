976550dc58e7d93925003a81e01dd3dc09442e67
Lukasz <lukasz.gibas96@gmail.com>
2026-01-25 21:16:24 +0100

    Libraries lecture exercise - name v3

---
+++ Week-4/name_v3.py
@@ -0,0 +1,11 @@
+# Libraries lecture exercise                            /sis.exit
+
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
+
+print("My name is", sys.argv[1])
