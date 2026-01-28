509bff1540a3b80f5ff010314363ea9d564a79f8
Lukasz <lukasz.gibas96@gmail.com>
2026-01-28 21:49:10 +0100

    Random from shorts

--- Week-4/Shorts/cards.py
+++ Week-4/Shorts/cards.py
@@ -1,11 +1,11 @@
-# random                            /random.choice()        /random.choices()       /random.sample()
+# random                            /random.choice()        /random.choices()       /random.sample()    /random.seed()
 import random
 
 cards = ["jack", "king", "queen"]
 
 
 def main():
-
+    #random.seed(0)
     print(random.choices(cards, weights = [75, 20 , 5], k=2))
 
 
