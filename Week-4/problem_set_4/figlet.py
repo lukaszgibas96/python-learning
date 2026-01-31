9e18021074b7c6d85b839c48316bd882f57331a8
Lukasz <lukasz.gibas96@gmail.com>
2026-01-31 18:30:01 +0100

    Problem set 4 - Frank, Ian and Glen's Letters

---
+++ Week-4/problem_set_4/figlet.py
@@ -0,0 +1,36 @@
+# Problem set 4 - Frank, Ian and Glen's Letters
+
+from pyfiglet import Figlet
+import sys
+import random
+
+figlet = Figlet()
+fonts = figlet.getFonts()
+
+def main():
+
+    if len(sys.argv) < 2:
+        f = random.choice(fonts)
+        convert(f)
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            if sys.argv[2] in fonts:
+                f = sys.argv[2]
+                convert(f)
+            else:
+                sys.exit("Invalid usage")
+        else:
+            sys.exit("Invalid usage")
+        
+    else:
+        sys.exit("Invalid usage")
+
+
+def convert(f):
+    text = input("Input: ")
+    figlet.setFont(font = f)
+    output = figlet.renderText(text)
+    print(output)
+
+
+main()
