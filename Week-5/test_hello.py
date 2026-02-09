8687db6e3d67ec907aea2abfaea55089487d81e7
Lukasz <lukasz.gibas96@gmail.com>
2026-02-09 17:55:51 +0100

    Remove old files

--- Week-5/test_hello.py
+++
@@ -1,8 +0,0 @@
-from hello import hello
-
-def test_default():
-    assert hello("Lukasz") == "hello, Lukasz"
-
-def test_argument():
-    for name in ["Lukasz", "Adam", "Kuba"]:
-        assert hello(name) == f"hello, {name}"
