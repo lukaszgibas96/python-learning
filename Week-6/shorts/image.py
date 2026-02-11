25d390b711d42803ddfab8bbdff3dd76c9d2b82b
Lukasz <lukasz.gibas96@gmail.com>
2026-02-11 19:05:32 +0100

    Pillow from shorts

--- Week-6/shorts/image.py
+++ Week-6/shorts/image.py
@@ -1,11 +1,14 @@
 # Shorts - pillow
 
 from PIL import Image
+from PIL import ImageFilter
+
  
 def main():
-    with Image.open("in.jpeg ") as img:
-        print(img.size)
-        print(img.format)
+    with Image.open("in.jpeg") as img:
+        img = img.rotate(180)
+        img = img.filter(ImageFilter.FIND_EDGES)
+        img.save("out.jpeg")
 
 
 main()
