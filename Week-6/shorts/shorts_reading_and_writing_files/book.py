527d795e3cb6df0fedee8d5a286fdf5a3fdd9072
Lukasz <lukasz.gibas96@gmail.com>
2026-02-11 21:15:07 +0100

    Reading and Writing Files from shorts

---
+++ Week-6/shorts/shorts_reading_and_writing_files/book.py
@@ -0,0 +1,16 @@
+# Reading and Writing Files from shorts /.read  /.readlines 
+
+def main():
+    with open("alice.txt", "r") as f:
+        contents = f.readlines()
+
+        chapter1 = contents[52:272]
+        print(chapter1)
+    with open("chapter1.txt", "w") as f:
+        f.writelines(chapter1)
+
+
+
+
+
+main()
