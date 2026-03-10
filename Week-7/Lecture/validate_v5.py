0ed09a3a342bd4cf92e4e944ac7b355d656e6d8e
Lukasz <lukasz.gibas96@gmail.com>
2026-03-10 19:10:08 +0100

    Exercise from lecture - validate v4

--- Week-7/Lecture/validate_v5.py
+++ Week-7/Lecture/validate_v5.py
@@ -3,8 +3,9 @@ import re
 email = input("What's your email?").strip()
 
 
-if re.search(r"^\w+@\w+\.edu$", email):
+if re.search(r"^\w+@\w+\.(edu|com|gov|net|org)$", email):
     print("Valid")
 else:
     print("Invalid ")
 
+ 
