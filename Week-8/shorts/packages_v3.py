a4af5ed16ee82c32d6dda81149b4e2d620766f4c
Lukasz <lukasz.gibas96@gmail.com>
2026-07-14 19:50:40 +0200

    Exercise from shorts - Instances Methods - packages_v3

---
+++ Week-8/shorts/packages_v3.py
@@ -0,0 +1,28 @@
+# shorts - classes
+
+class Package:
+
+    def __init__(self, number, sender, recipient, weight):
+        self.number = number
+        self.sender = sender
+        self.recipient = recipient
+        self.weight = weight
+    
+    def __str__(self):
+        return f"Package {self.number}: {self.sender} to {self.recipient}, {self.weight}kg"
+         
+
+
+def main():
+    #insted of this: packages = ["Package 1: Alice to Bob, 10kg", "Package 2: Bob to Charlie, 5kg"]
+    #do that:
+
+    packages = [
+
+        Package(number = 1, sender = "Alice", recipient = "Bob", weight = 10),
+        Package(number = 2, sender = "Bob", recipient = "Charlie", weight = 5)
+    ]
+    for package in packages:
+        print(package)
+
+main() 
