1fe4a7090761e53658724e601335bfa4a40bd4dc
Lukasz <lukasz.gibas96@gmail.com>
2026-07-28 18:06:39 +0200

    Problem set 8 - cookie jar

---
+++ Week-8/problem_set/jar.py
@@ -0,0 +1,34 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if self.capacity <= 0:
+            raise ValueError
+        self._capacity = capacity
+        self.cookies = 0
+
+        
+
+    def __str__(self):
+        return self.cookies * "🍪"
+
+    def deposit(self, n):
+        if n < 0:
+            raise ValueError
+        if self.cookies + n > self.capacity:
+            raise ValueError
+        
+        self.cookies = self.cookies + n
+
+    def withdraw(self, n):
+        if n < 0:
+            raise ValueError
+        if self.cookies - n < 0:
+            raise ValueError
+        self.cookies = self.cookies - n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self.cookies
